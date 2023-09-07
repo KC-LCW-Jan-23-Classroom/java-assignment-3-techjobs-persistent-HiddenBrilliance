8de1e8fb1b7b34ea629d1f22b8e9ddd3b01602b7
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2023-09-07 17:58:49 -0500

    added Autowired employerRepository into EmployerController

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotEmpty
+    @Size(max = 255)
+    private String location;
 
+    public Employer() {
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
